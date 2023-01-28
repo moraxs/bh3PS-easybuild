ba613e6514ab1903c077f200771e149b4cdb7e84
moraxs <98573765+moraxs@users.noreply.github.com>
2023-01-28 13:01:17 +0800

    Add files via upload

---
+++ Main_services/server/packetsHandler/GetAvatarTeamDataReq.ts
@@ -0,0 +1,19 @@
+import net from "net"
+import { GetAvatarTeamDataReq, GetAvatarTeamDataRsp, GetAvatarTeamDataRsp_CmdId, GetAvatarTeamDataRsp_Retcode } from "../../BengHuai"
+import Packet from "../Packet"
+
+export default (socket: net.Socket, packet: GetAvatarTeamDataReq) => {
+    Packet.getInstance().serializeAndSend(socket, GetAvatarTeamDataRsp_CmdId.CMD_ID, {
+        retcode: GetAvatarTeamDataRsp_Retcode.SUCC,
+        avatarTeamList: [
+            {
+                stageType: 1,
+                avatarIdList: [
+                    101,
+                    105,
+                    317
+                ]
+            }
+        ],
+    } as GetAvatarTeamDataRsp)
+}
